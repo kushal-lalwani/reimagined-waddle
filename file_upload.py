c33b1aa50c2850925860d0aba74283fecdaff2d1
Kushal Lalwani <109090706+kushal-lalwani@users.noreply.github.com>
2024-07-22 00:48:47 +0530

    Add files via upload

--- file_upload.py
+++ file_upload.py
@@ -1,19 +1,71 @@
 import os
-from flask import Flask, request, jsonify
+import boto3
+from flask import Flask, request, jsonify , session
 from utils import upload_to_s3,store_metadata
 from postgres_config import connect_to_db
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'Uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.secret_key = 'SECRET'
 
+@app.route('/login', methods=['POST'])
+def login():
+    access_key = request.form['access_key']
+    secret_key = request.form['secret_access_key']
+    session['credentials'] = {
+        'access_key': access_key,
+        'secret_access_key': secret_key
+    }
+    try:
 
+        sts_client = boto3.client(
+            'sts',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
+        )
+        sts_response = sts_client.get_caller_identity()
+        return jsonify({
+            'message': 'Login successful!',
+            'user_arn': sts_response['Arn'],
+            'user_id': sts_response['UserId']
+        }), 200
+
+    except (NoCredentialsError, PartialCredentialsError, ClientError) as e:
+        return jsonify({'message': f'Invalid credentials: {str(e)}'}), 401
+
+@app.route('/list-buckets', methods=['GET'])
+def list_buckets():
+    if 'credentials' not in session:
+        return jsonify({"error": "AWS credentials are required. Please log in first."}), 401
+
+    credentials = session['credentials']
+
+    try:
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=credentials['access_key'],
+            aws_secret_access_key=credentials['secret_access_key']
+        )
+
+        response = s3_client.list_buckets()
+        buckets = [bucket['Name'] for bucket in response['Buckets']]
+        return jsonify({'buckets': buckets}), 200
+
+    except Exception as e:
+        return jsonify({'message': f'Error listing buckets: {str(e)}'}), 500
 
 @app.post('/upload')
-def get_file_paths():
+def uploadFiles():
     files = request.files.getlist('files')
     ids = request.form.getlist('ids')
+    bucket_name = request.form['bucket_name']
+    credentials = session['credentials']
+    if 'credentials' not in session:
+        return jsonify({"error": "AWS credentials are required. Please log in first."}), 401
+
     uploaded_files = []
 
     if len(files) != len(ids):
@@ -27,13 +79,69 @@ def get_file_paths():
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
 
-        upload_to_s3(file_path, file_id)
+        upload_to_s3(bucket_name,file_path, file_id,credentials)
         store_metadata(conn, file_path,file_id)
         uploaded_files.append({"id": file_id, "filename": file.filename})
 
-        # os.remove(file_path)
+        os.remove(file_path)
 
     conn.close()
     return jsonify({"message": "Files uploaded successfully", "files": uploaded_files})
+
+@app.route('/list-objects', methods=['POST'])
+def list_objects():
+    credentials = session['credentials']
+    if 'credentials' not in session:
+        return jsonify({"error": "AWS credentials are required. Please log in first."}), 401
+
+    data = request.json
+    bucket_name = data.get('bucket_name')
+    folder = data.get('folder', '')
+    try:
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=credentials['access_key'],
+            aws_secret_access_key=credentials['secret_access_key']
+        )
+
+        response = s3_client.list_objects_v2(
+            Bucket=bucket_name,
+            Prefix=folder,
+            Delimiter='/'
+        )
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/generate-presigned-url', methods=['POST'])
+def generate_presigned_url():
+    credentials = session['credentials']
+    if 'credentials' not in session:
+        return jsonify({"error": "AWS credentials are required. Please log in first."}), 401
+
+    data = request.json
+    bucket_name = data.get('bucket_name')
+    object_key = data.get('object_key')
+
+    if not bucket_name or not object_key:
+        return jsonify({'error': 'Bucket name and object key are required'}), 400
+
+    try:
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=credentials.get('access_key'),
+            aws_secret_access_key=credentials.get('secret_access_key'),
+            region_name='ap-south-1'
+        )
+
+        url = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': bucket_name, 'Key': object_key},
+            ExpiresIn=3600*12
+        )
+
+        return jsonify({'url': url})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
 import os
 import boto3
-from datetime import datetime
-from postgres_config import connect_to_db
+from datetime import datetime,timezone
 from psycopg2 import sql
-
-BUCKET_NAME = 'files-bucket-b'
+import mimetypes
+BUCKET_NAME='files-bucket-a'
 REGION = 'ap-south-1'
 file_folder = {
         '1':'Folder-1',
@@ -12,15 +11,24 @@ file_folder = {
         '3':'Folder-3',
         '4':'Folder-4',
         '5':'Folder-5'
-    }
-def upload_to_s3(file_path, file_id):
-    s3_client = boto3.client('s3')
+}
+
+def upload_to_s3(bucket_name,file_path, file_id, credentials):
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=credentials['access_key'],
+        aws_secret_access_key=credentials['secret_access_key']
+    )
+
     file_name = os.path.basename(file_path)
+    s3_path = f"{file_folder.get(file_id)}/{file_name}"
+    mime_type,_=mimetypes.guess_type(file_path)
 
     try:
-        s3_client.upload_file(file_path, BUCKET_NAME, f"{file_folder.get(file_id)}/{file_name}")
+        s3_client.upload_file(file_path, bucket_name, s3_path,ExtraArgs={'ContentType':mime_type})
     except Exception as e:
-        print(e)
+        raise RuntimeError(f"Error uploading file {file_name} to S3: {str(e)}")
+
 
 def store_metadata(conn, file_path, file_id):
     cur = conn.cursor()
@@ -29,18 +37,21 @@ def store_metadata(conn, file_path, file_id):
         file_name = os.path.basename(file_path)
         file_size = os.path.getsize(file_path)
         file_extension = file_name.split('.')[-1]
-        folder_url = f"https://{BUCKET_NAME}.s3.{REGION}.amazonaws.com/{file_folder.get(file_id)}/"
-        file_url = f"{folder_url}{file_name}"
-        upload_datetime = datetime.now()
+
+        file_url = f"https://{BUCKET_NAME}.s3.{REGION}.amazonaws.com/{file_folder.get(file_id)}/{file_name}"
+        upload_datetime = datetime.now(timezone.utc)
+        print(upload_datetime)
 
         query = sql.SQL("""
             INSERT INTO metadata.file_metadata 
-            (file_name, file_size, file_extension, folder_url, file_url, upload_datetime) 
+            (file_name, file_size, file_extension, folder, file_url, upload_datetime) 
             VALUES (%s, %s, %s, %s, %s, %s)
         """)
-        cur.execute(query, (file_name, file_size, file_extension, folder_url, file_url, upload_datetime))
+        cur.execute(query, (file_name, file_size, file_extension, file_folder.get(file_id), file_url, upload_datetime))
         conn.commit()
 
         print("File metadata stored successfully")
     except Exception as e:
         print(f"Error storing file metadata: {e}")
+
+
